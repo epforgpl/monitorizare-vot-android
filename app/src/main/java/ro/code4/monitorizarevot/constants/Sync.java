81c1c40ff0c2373133f1a62538e9dec91c65eae2
Krzysiek Madejski <krzysztof.madejski@epf.org.pl>
2018-10-07 02:39:20 +0300

    Not conflicting Content Providers, relates to #68

--- app/src/main/java/ro/code4/monitorizarevot/constants/Sync.java
+++ app/src/main/java/ro/code4/monitorizarevot/constants/Sync.java
@@ -1,6 +1,8 @@
 package ro.code4.monitorizarevot.constants;
 
+import ro.code4.monitorizarevot.BuildConfig;
+
 public class Sync {
-    public static final String AUTHORITY = "ro.code4.monitorizarevot";
+    public static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".contentprovider";
     public static final String ACCOUNT_TYPE = "code4.ro";
 }
